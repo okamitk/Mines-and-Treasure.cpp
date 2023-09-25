e2945230695ef326328cc9b8df11dbe668718fd6
Okamitk <felipeoliveira53524@gmail.com>
2023-09-25 19:14:01 -0300

    Added Dialogs and refined the code

--- Main.cpp
+++ Main.cpp
@@ -1,37 +1,40 @@
 #include <iostream>
 #include <cstdlib>
 
+//#####################################################
+
 //The game is set on a matrix called world
 //Every space in world is called a block
 //A block can contain a mine or a treasure or nothing
 // 0 = empty block "█"
 // 1 = mine "█"
 // 2 = digged block " "
-// 3 = digged mine block "✕"
-// 4 = near mine block "⚠"
+// 3 = digged mine block "X"
+// 4 was supposed to be a ! sign, but was replaced by the amount of mines
 // 5 = treasure "█"
 // 6 = near treasure block "✦"
 // 7 = digged treasure block "★"
 
+//##################################################
 
 using str = std::string;
+
 const str END = "\n";
+
 struct dimensions
 {
     int height;
     int width;
 };
 
-bool checkExecution(){
-    return 1;
-}
+//#################################################
 
 int random(int offset, int range)
 {
     return(offset+(rand()%range));
 };
 
-int* getUserInput(str message, int amount)//Wont be used if implemented on GUI
+int* input(str message, int amount)//Wont be used if implemented on GUI
 {
     int* input = new int[amount];
 
@@ -41,104 +44,215 @@ int* getUserInput(str message, int amount)//Wont be used if implemented on GUI
         std::cin>>input[i];
     }
     return input;
-}
+};
 
-void checkNearBlocks(int **world, int x, int y)
-{
-    bool nearDanger=0;
-    bool nearTreasure=0;
-    int* mine;
-    int* treasure;
-    int chance;
-    int NEAREST_BLOCKS[8][2] =
-                  {{y-1,x-1},{y,x-1},{y+1,x-1},
-                   {y-1,x  },        {y+1,x  },
-                   {y-1,x+1},{y,x+1},{y+1,x+1}};
+bool isValid(int x, int y, int width, int height){
+    return(x>-1 && x<width && y>-1 && y<height);
+};
 
-    std::cout<<"Current block: "<<world[x][y]<<END;
-    for(int* block:NEAREST_BLOCKS)
-    {
-        std::cout<<"Near Block: "<<block[1]<<" "<<block[0]<<END;   
-        if(block[1]>-1 && block[2]>-1){         
-            if(world[block[1]][block[0]]==1)
+//#################################################
+
+void deathDialog(int chance){
+    str Dialogs[5] =
             {
-                std::cout<<"This block contains a mine"<<END;
-                nearDanger=1;
-                mine=block;
-            }
-            if (world[block[1]][block[0]]==5)
-            {
-                std::cout<<"This block contains the treasure"<<END;
-                nearTreasure=1;
-                treasure=block; 
-            }
-        }
-    }
+                "Enquanto procura pelo tesouro, a última coisa que você ouve é um clique. \n\n Você morreu.",
+                "Enquanto procurava pelo tesouro, você atingiu uma mina com uma pá, ela deveria estar desativada já que você estava pisando nela o tempo todo... Mas então você ouviu um clique. \n\n Você morreu.",
+                "Você encontrou uma coisa de metal, mas acontece que não era realmente um tesouro. Bem, venderia por um pouco de prata se não estivesse prestes a explodir. \n\n Você morreu, ",
+                "Você pisou em uma mina. No final a busca pelo tesouro não valeu muita coisa...\n\n Você morreu.",
+                "Enquanto procurava pelo tesouro, você atingiu uma mina com sua pá. 5 segundos se passaram como se fossem horas, mas nada aconteceu.\n\n Você recebeu uma nova chance."
+            };
+    std::cout << 
+        " _____                        _____                \n"
+        "|  __ \\                      |  _  |               \n"
+        "| |  \\/ __ _ _ __ ___   ___  | | | |_   _____ _ __ \n"
+        "| | __ / _` | '_ ` _ \\ / _ \\ | | | \\ \\ / / _ \\ '__|\n"
+        "| |_\\ \\ (_| | | | | | |  __/ \\ \\_/ /\\ V /  __/ |   \n"
+        " \\____/\\__,_|_| |_| |_|\\___|  \\___/  \\_/ \\___|_|         \n"
+        "";
+    std::cout << END << END << Dialogs[chance] << END;
+};
 
-    if(nearDanger)
+void digDialog(){
+    str Dialogs[5] =
+            {
+                "Você cavou, cavou e cavou mas não encontrou nada.",
+                "Depois de cavar por algum tempo você não encontrou nada.",
+                "Não há nada aqui.",
+                "Cavando por vários minutos, a única coisa que você encontrou foi terra e pedras, muitas pedras.",
+                "Você cavou, mas não há nada valioso."
+            };
+    std::cout << END << END << Dialogs[random(0,4)] << END;
+};
+
+void nearDangerDialog(int chance){
+    str  Dialogs[5] =
+            {
+                "Você pode sentir o cheiro de pólvora vindo das paredes da escavação. Há uma mina por perto.",
+                "Você sente um calafrio horrível. Tem algo próximo.",
+                "Você consegue ouvir o impacto da sua pá ecoando em alguma mina próxima.",
+                "Há polvora misturada na areia que você acabou de cavar. Tem um bomba por perto",
+                "Você bateu forte demais com a pá e sentiu o chão tremer, uma mina explodiu por perto."
+            };
+    std::cout << END << END << Dialogs[chance] << END;
+};
+
+void nearTreasureDialog(){
+    str Dialogs[5] =
+            {
+                "Antes de sair você encontrou uma moeda de ouro sob seu pé, talvez o tesouro esteja próximo.",
+                "Depois de inspecionar com mais cuidado, você encontrou a peça de um relógio de ouro, talvez o tesouro esteja próximo.",
+                "Um pó dourado misturado na areia brilha enquanto você sai do buraco, o tesouro deve estar próximo.",
+                "Havia uma pequena pérola brilhando na terra que você acabou de cavar, o tesouro está próximo.",
+                "Depois de olhar com mais cuidado você vê há um anel de prata na terra. O tesouro deve estar próximo."
+            };
+    std::cout << END << END << Dialogs[random(0,4)] << END;
+};
+
+void TreasureDialog(){
+    str Dialogs[5] =
+            {
+                "Depois de encontrar um baú, você ficou tão aliviado que cai de joelhos e sente que poderia morrer em paz agora, bem, não exatamente agora.\n\n Você encontrou o tesouro",
+                "Você encontrou um baú contendo várias joias e algumas moedas antigas. Você pode viver bem agora\n\n Você encontrou o tesouro.",
+                "Você encontrou um baú contendo muitas moedas de ouro. A moeda em si não é utilizável, mas o ouro sim. Você pode viver em paz agora\n\n Você encontrou o tesouro.",
+                "Você encontrou uma caixa de madeira coom várias barras de ouro. Você agora, não precisando mais arriscar sua própria vida, vai para casa com cuidado.\n\n Você encontrou o tesouro.",
+                "Você acerta um pedaço de metal com sua pá e toda a sua vida passa diante de seus olhos. Depois de quase desmaiar de medo, você percebe que a tal caixa tem na verdade milhões em pérolas. Você agora vive o resto de sua vida em paz.\n\n Você encontrou o tesouro."
+            };
+    std::cout <<
+        "__   __            _    _ _       _ \n"
+        "\\ \\ / /           | |  | (_)     | |\n"
+        " \\ V /___  _   _  | |  | |_ _ __ | |\n"
+        "  \\ // _ \\| | | | | |/\\| | | '_ \\| |\n"
+        "  | | (_) | |_| | \\  /\\  / | | | |_|\n"
+        "  \\_/\\___/ \\__,_|  \\/  \\/|_|_| |_(_)";
+    std::cout << Dialogs[random(0,4)];
+};
+
+//###################################################
+
+void check(int** world, int x, int y, int width, int height){
+    int near[8][2] =
+                {
+                {y-1,x-1}, {y,x-1}, {y+1,x-1},
+                {y-1,x  },          {y+1,x  },
+                {y-1,x+1}, {y,x+1}, {y+1,x+1}
+                };
+
+    world[x][y] = 2;
+    for(int* block:near)
     {
-        chance = random(0,4);
-        if(chance = 4 && world[x][y]!=4)
+        if(isValid(x, y, width, height))
         {
-            world[x][y]=4;
-            world[mine[1]][mine[0]]=3;
-            checkNearBlocks(world, x, y);
-        }
-        
-    }
-    if(nearTreasure)
-    {
-        chance = random(0,4);
-        world[x][y]=6;
-        if(chance==4)
-        {
-            world[treasure[1]][treasure[0]]=7;
+            if(world[block[1]][block[0]] == 1)
+            {
+                world[x][y] = 4;
+            }
+            else if(world[block[1]][block[0]] == 5){
+                world[x][y] = 6;
+            };
         };
+    };
+};
+
+void checkNear(int **world, int x, int y, int* previous_exploded, int width, int height)
+{
+    int mines = 0;
+    int near[8][2] =
+                {
+                {y-1,x-1},{y,x-1},{y+1,x-1},
+                {y-1,x  },        {y+1,x  },
+                {y-1,x+1},{y,x+1},{y+1,x+1}
+                };
+    bool nearDanger = 0;
+    bool nearTreasure = 0;
+    int* mine;
+
+    check(world, previous_exploded[1], previous_exploded[0], width, height);
+    for(int* block:near)
+    {
+        if(isValid(block[1], block[0], width, height))
+        {         
+            if(world[block[1]][block[0]] == 1)
+            {
+                nearDanger = 1;
+                mine = block;
+            }
+            else if(world[block[1]][block[0]] == 5)
+            {
+                nearTreasure = 1;
+            }
+        }
+        if(mines>0){
+            world[x][y]<<mines;
+        }
+    }
+    if(nearDanger){
+        if(random(0,6) == 4  && world[x][y]!= 4)
+                {
+                    world[mine[1]][mine[0]] = 3;
+                    nearDangerDialog(4);
+                    check(world, x, y, width, height);
+                    previous_exploded = mine;
+                }
+                else
+                {
+                    mines++;
+                    nearDangerDialog(random(0,3));
+                }
+    } 
+    else if(nearTreasure)
+    {
+        world[x][y] = 6;
+        nearTreasureDialog();
+    }
+    else
+    {
+        digDialog();
     }
 };
 
-void digBlock(int**world, int x, int y, bool alive, bool rich)
+void digBlock(int**world, int x, int y, bool alive, bool rich, int* previous_exploded, int width, int height)
 {
-    alive = (world[x][y]!=1);
-
-    std::cout<<"Alive: "<<alive<<END;
-    std::cout<<"Block: "<<world[x][y]<<END;
-    std::cout<<"Coord: "<<x<<" "<<y<<END;
-
-    if(alive)
-    {
         if(world[x][y]==5)
         {
             world[x][y]==7;
             rich = 1;
+
         }
         else
         {
             world[x][y]=2;
-            checkNearBlocks(world, x, y);
-
-            std::cout<<"Alive: "<<alive<<END;
-            std::cout<<"Block: "<<world[x][y]<<END;
-            std::cout<<"Coord: "<<x<<" "<<y<<END;
+            checkNear(world, x, y, previous_exploded, width, height);
         };
-    };
 }
 
-bool isValid(int x, int y, int width, int height){
-    return(x>-1 && x<width+1 && y>-1 && y<height+1);
-};
-
-bool chooseBlock(int** world, bool alive, bool rich, int width, int height)//Wont be used if implemented on GUI
+bool chooseBlock(int** world, bool alive, bool rich, int width, int height, int* previous_exploded)//Wont be used if implemented on GUI
 {   
-    int x=(getUserInput("Digite a coluna do bloco: ",1))[0];
-    int y=(getUserInput("Digite a linha do bloco: ",1))[0];
+    int x = (input("Digite a coluna do bloco: ",1))[0];
+    int y = (input("Digite a linha do bloco: ",1))[0];
+    int chance = random(0,4);
     
-    if(isValid(x,y,width, height)){
-        digBlock(world,x-1,y-1,alive,rich);
+    if(isValid(x, y, width, height))
+    {
+        alive = (world[x][y]!=1);
+        if(alive && !rich)
+        {
+            digBlock(world, x-1, y-1, alive, rich, previous_exploded, width, height);
+        }
+        else if(!alive)
+        {
+            if(chance==4){
+                alive=1;
+            }
+            deathDialog(chance);
+        }
+        else if(rich)
+        {
+            TreasureDialog();
+        }
     }
     else
     {
-        std::cout<<"Essas coordenadas não são válidas."<<END;
+        std::cout << "Essas coordenadas não são válidas." << END;
     }
     
     return 1;
@@ -146,10 +260,10 @@ bool chooseBlock(int** world, bool alive, bool rich, int width, int height)//Won
 
 void printWorld(int** world, int width, int height)
 {
-    for(int i=0; i<height; i++)
+    for(int i = 0; i < height; i++)
     {
-        std::cout<<i+1<<" ";
-        for(int j=0; j<width; j++)
+        std::cout << i+1 << " ";
+        for(int j = 0; j < width; j++)
         {
             // std::cout<<world[i][j];
             switch (world[i][j])
@@ -166,9 +280,6 @@ void printWorld(int** world, int width, int height)
                 case 3:
                     std::cout<<"X";
                     break;
-                case 4:
-                    std::cout<<"!";
-                    break;
                 case 5:
                     std::cout<<"█";
                     break;
@@ -179,7 +290,7 @@ void printWorld(int** world, int width, int height)
                     std::cout<<"★";
                     break;
                 default:
-                    std::cout<<"█";
+                    std::cout<<world[i][j];
                     break;
             }
         }
@@ -237,6 +348,7 @@ void **generateWorld(int **world, int width, int height)
         }
     }
     //Treasure
+    check(world, 1, 1, width, height);
     world[random(0,height)][random(0,width)]=5;
 };
 
@@ -245,12 +357,18 @@ int main()
     int**world;
     bool rich=0;
     bool alive=1;
+    int previous_exploded[2] = {1,1};
 
-    int difficulty=(getUserInput("Dificuldade:\n1: Fácil\n2: Médio\n3: Difícil",1))[0];
+    int difficulty=(input("Dificuldade:\n1: Fácil\n2: Médio\n3: Difícil",1))[0];
     dimensions world_dimensions = selectDimensions(difficulty);
     world = generateMatrix(world_dimensions.width, world_dimensions.height);
     generateWorld(world, world_dimensions.width, world_dimensions.height);
 
+    std::cout << END << "        _                                   _   _____                                        \n"
+        "  /\\/\\ (_)_ __   ___  ___    __ _ _ __   __| | /__   \\_ __ ___  __ _ ___ _   _ _ __ ___  ___ \n"
+        " /    \\| | '_ \\ / _ \\/ __|  / _` | '_ \\ / _` |   / /\\/ '__/ _ \\/ _` / __| | | | '__/ _ \\/ __|\n"
+        "/ /\\/\\ \\ | | | |  __/\\__ \\ | (_| | | | | (_| |  / /  | | |  __/ (_| \\__ \\ |_| | | |  __/\\__ \\\n"
+        "\\/    \\/_|_| |_|\\___||___/  \\__,_|_| |_|\\__,_|  \\/   |_|  \\___|\\__,_|___/\\__,_|_|  \\___||___/"<<END;
     std::cout<<"Tem um tesouro e várias minas nesse terreno, selecione um bloco para cavar."<<END;
     printWorld(world, world_dimensions.width, world_dimensions.height);
 
@@ -258,7 +376,7 @@ int main()
         bool choosen=0;
         while(!choosen){
             try{
-                if(chooseBlock(world, alive, rich, world_dimensions.width, world_dimensions.height))
+                if(chooseBlock(world, alive, rich, world_dimensions.width, world_dimensions.height, previous_exploded))
                 {
                     choosen=1;
                 }
